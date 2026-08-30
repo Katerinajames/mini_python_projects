ac65329f52c27f7942f9755fd7981fe795eb3b75
Katerina <76427845+Katerinajames@users.noreply.github.com>
2026-08-30 14:42:11 +0300

    Add files via upload

---
+++ dark_outside.py
@@ -0,0 +1,6 @@
+is_dark = input("Is it dark outside? (y/n): ").lower().strip()
+
+if is_dark in ("y", "yes", "yeah", "yep"):
+    print("ZZZZZZ   😴   good night")
+else:
+    print("Okay, still party time! 🎉")
